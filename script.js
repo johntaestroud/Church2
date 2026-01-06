0979fe6ee8dad9841e2002c34a008faaa5e8ff1c
Johntae Stroud <johntae.stroud@gmail.com>
2026-01-05 22:02:32 -0500

    hover target commented

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 (function ($) {
     "use strict";
 
-    //Page cursors
-    document.getElementsByTagName("body")[0].addEventListener("mousemove", function (n) {
-        t.style.left = n.clientX + "px",
-            t.style.top = n.clientY + "px",
-            e.style.left = n.clientX + "px",
-            e.style.top = n.clientY + "px",
-            i.style.left = n.clientX + "px",
-            i.style.top = n.clientY + "px";
-    });
-    var t = document.getElementById("cursor"),
-        e = document.getElementById("cursor2"),
-        i = document.getElementById("cursor3");
-    function n(t) {
-        e.classList.add("hover"), i.classList.add("hover");
-    }
-    function s(t) {
-        e.classList.remove("hover"), i.classList.remove("hover");
-    }
-    s();
-    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
-        if (window.CP.shouldStopExecution(0)) break;
-        o(r[a]);
-    } window.CP.exitedLoop(0);
-    function o(t) {
-        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s);
-    }
+    //Page cursors hover-target
+    // document.getElementsByTagName("body")[0].addEventListener("mousemove", function (n) {
+    //     t.style.left = n.clientX + "px",
+    //         t.style.top = n.clientY + "px",
+    //         e.style.left = n.clientX + "px",
+    //         e.style.top = n.clientY + "px",
+    //         i.style.left = n.clientX + "px",
+    //         i.style.top = n.clientY + "px";
+    // });
+    // var t = document.getElementById("cursor"),
+    //     e = document.getElementById("cursor2"),
+    //     i = document.getElementById("cursor3");
+    // function n(t) {
+    //     e.classList.add("hover"), i.classList.add("hover");
+    // }
+    // function s(t) {
+    //     e.classList.remove("hover"), i.classList.remove("hover");
+    // }
+    // s();
+    // for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+    //     if (window.CP.shouldStopExecution(0)) break;
+    //     o(r[a]);
+    // } window.CP.exitedLoop(0);
+    // function o(t) {
+    //     t.addEventListener("mouseover", n), t.addEventListener("mouseout", s);
+    // }
 
 
     //About page
