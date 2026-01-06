7228c271a7d4b1eb0c38bc9e18bc5c65d728e8ca
Johntae Stroud <johntae.stroud@gmail.com>
2026-01-06 00:08:40 -0500

    preloader

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 (function ($) {
     "use strict";
+    //PRELOADER START
+    $(document).ready(function() {
+    
+    setTimeout(function() {
+        $('#ctn-preloader').addClass('loaded');
+        $('body').removeClass('no-scroll-y');
+
+        if ($('#ctn-preloader').hasClass('loaded')) {
+        $('#preloader').delay(1000).queue(function() {
+            $(this).remove();
+        });
+        }
+    }, 3000);
+    
+    });
+    //PRELOADER END
 
     //Page cursors hover-target
     // document.getElementsByTagName("body")[0].addEventListener("mousemove", function (n) {
