064810c11d0c37805dd41acbb7bfc380c15c63a7
Johntae Stroud <johntae.stroud@gmail.com>
2026-01-06 23:54:09 -0500

    preloader

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
     $(document).ready(function() {
     
     setTimeout(function() {
-        $('#ctn-preloader').addClass('loaded');
+        $('#container').addClass('loaded');
         $('body').removeClass('no-scroll-y');
 
-        if ($('#ctn-preloader').hasClass('loaded')) {
-        $('#preloader').delay(1000).queue(function() {
+        if ($('#container').hasClass('loaded')) {
+        $('#container').delay(1000).queue(function() {
             $(this).remove();
         });
         }
